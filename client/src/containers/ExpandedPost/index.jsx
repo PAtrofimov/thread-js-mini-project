31a6f18f69b6487dea4fe2dc48c9e58d92ad4716
Peter Trofimov <ptrofimov7@gmail.com>
2020-06-06 22:34:05 +0300

    Added output UpdatedPost

--- client/src/containers/ExpandedPost/index.jsx
+++ client/src/containers/ExpandedPost/index.jsx
@@ -11,12 +11,15 @@ import {
   toggleExpandedPost,
   toggleUpdatedPost,
   toggleDeletedPost,
-  addComment
+  addComment,
+  updateComment,
+  toggleUpdatedComment,
 } from "src/containers/Thread/actions";
 import Post from "src/components/Post";
 import Comment from "src/components/Comment";
 import AddComment from "src/components/AddComment";
 import Spinner from "src/components/Spinner";
+import UpdatedComment from 'src/containers/UpdatedComment';
 
 const ExpandedPost = ({
   post,
@@ -26,53 +29,71 @@ const ExpandedPost = ({
   toggleExpandedPost: toggle,
   toggleUpdatedPost: toggleUpdated,
   toggleDeletedPost: toggleDeleted,
-  addComment: add
+  toggleUpdatedComment: toggleUpdatedCom,
+  addComment: add,
+  updateComment: update,
+  comment: updatedComment,
+  userId
 }) => (
-  <Modal dimmer="blurring" centered={false} open onClose={() => {toggle();}}>
-    {post ? (
-      <Modal.Content>
-        <Post
-          post={post}
-          likePost={like}
-          dislikePost={dislike}
-          toggleExpandedPost={toggle}
-          toggleUpdatedPost={toggleUpdated}
-          toggleDeletedPost={toggleDeleted}
-          sharePost={sharePost}
-        />
-        <CommentUI.Group style={{ maxWidth: "100%" }}>
-          <Header as="h3" dividing>
-            Comments
+    <Modal dimmer="blurring" centered={false} open onClose={() => { toggle(); }}>
+      {post ? (
+        <Modal.Content>
+          <Post
+            post={post}
+            likePost={like}
+            dislikePost={dislike}
+            toggleExpandedPost={toggle}
+            toggleUpdatedPost={toggleUpdated}
+            toggleDeletedPost={toggleDeleted}
+            sharePost={sharePost}
+          />
+          <CommentUI.Group style={{ maxWidth: "100%" }}>
+            <Header as="h3" dividing>
+              Comments
           </Header>
-          {post.comments &&
-            post.comments
-              .sort((c1, c2) => moment(c1.createdAt).diff(c2.createdAt))
-              .map((comment) => <Comment key={comment.id} comment={comment} />)}
-          <AddComment postId={post.id} addComment={add} />
-        </CommentUI.Group>
-      </Modal.Content>
-    ) : (
-      <Spinner />
-    )}
-  </Modal>
-);
+            {post.comments &&
+              post.comments
+                .sort((c1, c2) => moment(c1.createdAt).diff(c2.createdAt))
+                .map((comment) => <Comment key={comment.id} comment={comment} toggleUpdatedComment={toggleUpdatedCom} />)}
+            <AddComment postId={post.id} addComment={add} />
+
+            {updatedComment && updatedComment.userId === userId && (
+              <UpdatedComment updateComment={update} />)}
+
+          </CommentUI.Group>
+        </Modal.Content>
+      ) : (
+          <Spinner />
+        )}
+    </Modal>
+  );
+
+ExpandedPost.defaultProps = {
+  post: undefined,
+  userId: undefined,
+  comment: undefined,
+};
 
 ExpandedPost.propTypes = {
   post: PropTypes.objectOf(PropTypes.any).isRequired,
   toggleExpandedPost: PropTypes.func.isRequired,
   toggleUpdatedPost: PropTypes.func.isRequired,
   toggleDeletedPost: PropTypes.func.isRequired,
+  toggleUpdatedComment: PropTypes.func.isRequired,
   likePost: PropTypes.func.isRequired,
   dislikePost: PropTypes.func.isRequired,
   addComment: PropTypes.func.isRequired,
+  updateComment: PropTypes.func.isRequired,
   sharePost: PropTypes.func.isRequired,
- };
+};
 
 const mapStateToProps = (rootState) => ({
   post: rootState.posts.expandedPost,
+  comment: rootState.posts.updatedComment,
+  userId: rootState.profile.user.id,
 });
 
-const actions = { likePost, dislikePost, toggleExpandedPost, addComment, toggleUpdatedPost, toggleDeletedPost,};
+const actions = { likePost, dislikePost, toggleExpandedPost, addComment, toggleUpdatedPost, toggleDeletedPost, updateComment, toggleUpdatedComment };
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
 
