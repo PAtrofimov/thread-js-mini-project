2fc22d3a64caea87f804a395a245cae5c492ed6a
Peter Trofimov <ptrofimov7@gmail.com>
2020-06-13 12:44:21 +0300

    Added route for CommentReactions

--- server/src/api/routes/commentRoutes.js
+++ server/src/api/routes/commentRoutes.js
@@ -10,11 +10,20 @@ router
   .post('/', (req, res, next) => commentService.create(req.user.id, req.body)
     .then(comment => res.send(comment))
     .catch(next))
-  .put('/:id', (req, res, next) => commentService.updateById(req.params.id, req.body)
-    .then(comment => res.send(comment))
-    .catch(next))
   .delete('/:id', (req, res, next) => commentService.deleteById(req.params.id)
     .then(result => res.send({ result }))
+    .catch(next))
+  .put('/react', (req, res, next) => commentService.setReaction(req.user.id, req.body)
+    .then(reaction => {
+      if (reaction.comment && (reaction.comment.userId !== req.user.id)) {
+        // notify a user if someone (not himself) liked his comment
+        req.io.to(reaction.comment.userId).emit('like', 'Your comment was liked!');
+      }
+      return res.send(reaction);
+    })
+    .catch(next))
+  .put('/:id', (req, res, next) => commentService.updateById(req.params.id, req.body)
+    .then(comment => res.send(comment))
     .catch(next));
 
 export default router;
