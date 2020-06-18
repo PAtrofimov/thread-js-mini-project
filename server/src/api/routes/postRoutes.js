8d1f8758081b4bb2eb293048041471a3589f863f
Peter Trofimov <ptrofimov7@gmail.com>
2020-06-18 10:12:24 +0300

    Added route for getting users liked/disliked

--- server/src/api/routes/postRoutes.js
+++ server/src/api/routes/postRoutes.js
@@ -2,40 +2,69 @@
 
 import { Router } from 'express';
 import * as postService from '../services/postService';
+import * as userService from '../services/userService';
 
 const router = Router();
 
 router
-  .get('/', (req, res, next) => postService.getPosts(req.query)
-    .then(posts => res.send(posts))
-    .catch(next))
-  .get('/:id', (req, res, next) => postService.getPostById(req.params.id)
-    .then(post => res.send(post))
-    .catch(next))
-  .post('/', (req, res, next) => postService.create(req.user.id, req.body)
-    .then(post => {
-      req.io.emit('new_post', post); // notify all users that a new post was created
-      return res.send(post);
-    })
-    .catch(next))
-  .delete('/:id', (req, res, next) => postService.deleteById(req.params.id)
-    .then(result => {
-      return res.send({result});
-    })
-    .catch(next))
-  .put('/react', (req, res, next) => postService.setReaction(req.user.id, req.body)
-    .then(reaction => {
-      if (reaction.post && (reaction.post.userId !== req.user.id)) {
-        // notify a user if someone (not himself) liked his post
-        req.io.to(reaction.post.userId).emit('like', 'Your post was liked!');
-      }
-      return res.send(reaction);
-    })
-    .catch(next))
-  .put('/:id', (req, res, next) => postService.updateById(req.params.id, req.body)
-    .then(post => {
-      return res.send(post);
-    })
-    .catch(next));
+  .get('/', (req, res, next) =>
+    postService
+      .getPosts(req.query)
+      .then(posts => res.send(posts))
+      .catch(next)
+  )
+  .get('/:id', (req, res, next) =>
+    postService
+      .getPostById(req.params.id)
+      .then(post => res.send(post))
+      .catch(next)
+  )
+  .post('/', (req, res, next) =>
+    postService
+      .create(req.user.id, req.body)
+      .then(post => {
+        req.io.emit('new_post', post); // notify all users that a new post was created
+        return res.send(post);
+      })
+      .catch(next)
+  )
+  .delete('/:id', (req, res, next) =>
+    postService
+      .deleteById(req.params.id)
+      .then(result => {
+        return res.send({ result });
+      })
+      .catch(next)
+  )
+
+  .get('/react/:id', (req, res, next) =>
+    userService
+      .getUsersOfPostByReaction(req.query)
+      .then(users => {
+        return res.send(users);
+      })
+      .catch(next)
+  )
+  .put('/react', (req, res, next) =>
+    postService
+      .setReaction(req.user.id, req.body)
+      .then(reaction => {
+        if (reaction.post && reaction.post.userId !== req.user.id) {
+          // notify a user if someone (not himself) liked his post
+          req.io.to(reaction.post.userId).emit('like', 'Your post was liked!');
+        }
+        return res.send(reaction);
+      })
+      .catch(next)
+  )
+
+  .put('/:id', (req, res, next) =>
+    postService
+      .updateById(req.params.id, req.body)
+      .then(post => {
+        return res.send(post);
+      })
+      .catch(next)
+  );
 
 export default router;
