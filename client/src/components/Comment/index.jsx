9ff762b47e6f45f2e624e1da5758e1a13e1ad4bf
Peter Trofimov <ptrofimov7@gmail.com>
2020-06-19 15:48:42 +0300

    Added popup with reacted users

--- client/src/components/Comment/index.jsx
+++ client/src/components/Comment/index.jsx
@@ -1,67 +1,122 @@
 /* eslint-disable */
-import React from 'react';
-import PropTypes from "prop-types";
-import { Comment as CommentUI, Label, Icon } from "semantic-ui-react";
-import moment from "moment";
-import { getUserImgLink } from "src/helpers/imageHelper";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Comment as CommentUI, Label, Icon } from 'semantic-ui-react';
+import moment from 'moment';
+import { getUserImgLink } from 'src/helpers/imageHelper';
+import {
+  showUsersByLikes,
+  showUsersByDislikes
+} from 'src/services/commentService';
+import { Popup } from 'semantic-ui-react';
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
-const Comment = ({ comment: { body, createdAt, user, id, postId, likeCount=0, dislikeCount=0 }, toggleUpdatedComment: toggle, toggleDeletedComment: toggleDel, likeComment, dislikeComment}) => (
+const Comment = ({
+  comment: {
+    body,
+    createdAt,
+    user,
+    id,
+    postId,
+    likeCount = 0,
+    dislikeCount = 0
+  },
+  toggleUpdatedComment: toggle,
+  toggleDeletedComment: toggleDel,
+  likeComment,
+  dislikeComment
+}) => {
+  const [usersLikedComment, setUsersLikedComment] = useState([]);
+  const [usersDislikedComment, setUserDislikedComment] = useState([]);
 
-  <CommentUI className={styles.comment}>
-    <CommentUI.Avatar src={getUserImgLink(user.image)} />
-    <CommentUI.Content>
-      <CommentUI.Author as="a">{user.username}</CommentUI.Author>
-      <CommentUI.Metadata>{moment(createdAt).fromNow()}</CommentUI.Metadata>
-      <CommentUI.Text>{body}</CommentUI.Text>
-      <CommentUI.Actions>
-        <Label
-          basic
-          size="small"
-          as="a"
-          className={styles.toolbarBtn}
-          onClick={() => likeComment(id, postId)}
-        >
-          <Icon name="thumbs up" />
-          {likeCount||0}
-        </Label>
-        <Label
-          basic
-          size="small"
-          as="a"
-          className={styles.toolbarBtn}
-          onClick={() => dislikeComment(id, postId)}
-        >
-          <Icon name="thumbs down" />
-          {dislikeCount||0}
-        </Label>
-        <Label
-          basic
-          size="small"
-          as="a"
-          className={styles.toolbarBtn}
-          onClick={() => toggle(id)}
-        >
-          <Icon name="edit" />
-        </Label>
-        <Label
-          basic
-          size="small"
-          as="a"
-          className={styles.toolbarBtn}
-          onClick={() => toggleDel(id)}
-        >
-          <Icon name="delete" />
-        </Label>
-      </CommentUI.Actions>
-    </CommentUI.Content>
-  </CommentUI>
+  useEffect(() => {
+    async function fetchUsers() {
+      const usersLiked = await showUsersByLikes(id);
+      setUsersLikedComment(usersLiked.map((it) => it.username));
+      const usersDisliked = await showUsersByDislikes(id);
+      setUserDislikedComment(usersDisliked.map((it) => it.username));
+    }
 
-);
+    fetchUsers();
+  }, [dislikeCount, likeCount]);
+
+  return (
+    <CommentUI className={styles.comment}>
+      <CommentUI.Avatar src={getUserImgLink(user.image)} />
+      <CommentUI.Content>
+        <CommentUI.Author as="a">{user.username}</CommentUI.Author>
+        <CommentUI.Metadata>{moment(createdAt).fromNow()}</CommentUI.Metadata>
+        <CommentUI.Text>{body}</CommentUI.Text>
+        <CommentUI.Actions>
+          <Label
+            basic
+            size="small"
+            as="a"
+            className={styles.toolbarBtn}
+            onClick={() => likeComment(id, postId)}
+          >
+            <Popup
+              className={styles.popup}
+              content={
+                usersLikedComment.length > 0
+                  ? usersLikedComment.join(', ')
+                  : 'No likes'
+              }
+              trigger={
+                <div>
+                  <Icon name="thumbs up" /> {likeCount || 0}
+                </div>
+              }
+            />
+          </Label>
+          <Label
+            basic
+            size="small"
+            as="a"
+            className={styles.toolbarBtn}
+            onClick={() => dislikeComment(id, postId)}
+          >
+            <Popup
+              className={styles.popup}
+              content={
+                usersDislikedComment.length > 0
+                  ? usersDislikedComment.join(', ')
+                  : 'No dislikes'
+              }
+              trigger={
+                <div>
+                  <Icon name="thumbs down" /> {dislikeCount || 0}
+                </div>
+              }
+            />
+          </Label>
+          <Label
+            basic
+            size="small"
+            as="a"
+            className={styles.toolbarBtn}
+            onClick={() => toggle(id)}
+          >
+            <Icon name="edit" />
+          </Label>
+          <Label
+            basic
+            size="small"
+            as="a"
+            className={styles.toolbarBtn}
+            onClick={() => toggleDel(id)}
+          >
+            <Icon name="delete" />
+          </Label>
+        </CommentUI.Actions>
+      </CommentUI.Content>
+    </CommentUI>
+  );
+};
 
 Comment.propTypes = {
-  comment: PropTypes.objectOf(PropTypes.any).isRequired,
+  comment: PropTypes.objectOf(PropTypes.any).isRequired
 };
 
 export default Comment;
