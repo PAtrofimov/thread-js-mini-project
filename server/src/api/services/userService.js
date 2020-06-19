e069916a2588e4e4b24a7e8ce1ec0024a076a2d7
Peter Trofimov <ptrofimov7@gmail.com>
2020-06-19 15:47:03 +0300

    Added service, repozitory for getting reacted users

--- server/src/api/services/userService.js
+++ server/src/api/services/userService.js
@@ -1,8 +1,18 @@
 import userRepository from '../../data/repositories/userRepository';
 
 export const getUserById = async userId => {
-  const { id, username, email, imageId, image } = await userRepository.getUserById(userId);
+  const {
+    id,
+    username,
+    email,
+    imageId,
+    image
+  } = await userRepository.getUserById(userId);
   return { id, username, email, imageId, image };
 };
 
-export const getUsersOfPostByReaction = ({postId, isLike}) => userRepository.getUsersOfPostByReaction(postId, isLike);
+export const getUsersOfPostByReaction = ({ postId, isLike }) =>
+  userRepository.getUsersOfPostByReaction(postId, isLike);
+
+export const getUsersOfCommentByReaction = ({ commentId, isLike }) =>
+  userRepository.getUsersOfCommentByReaction(commentId, isLike);
--- server/src/data/repositories/userRepository.js
+++ server/src/data/repositories/userRepository.js
@@ -22,6 +22,23 @@ class UserRepository extends BaseRepository {
     });
   }
 
+  getUsersOfCommentByReaction(commentId, isLike = true) {
+    const where = {};
+
+    Object.assign(where, {
+      [Op.and]: [
+        sequelize.literal(`id in (select "commentReactions"."userId" 
+            from "commentReactions" as "commentReactions"  
+            WHERE "commentReactions"."commentId" = '${commentId}' and "commentReactions"."isLike" = ${isLike})`)
+      ]
+    });
+
+    return this.model.findAll({
+      where,
+      attributes: ['id', 'username']
+    });
+  }
+
   addUser(user) {
     return this.create(user);
   }
